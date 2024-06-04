11bc86f0ecf08258db782f86c86932423e7981b3
krupalidholakiya <krupalidholakiya85@gmail.com>
2024-06-04 16:59:13 +0530

    add file

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import img1 from './images/img1.avif'
+
+function App() {
+  return (
+    <>
+    <header>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li><div className=""></div>
+          <li>Product</li>
+        </ul>
+      </nav>
+      <p className='p1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore neque quo quas molestiae deleniti, perferendis omnis dolore quam consectetur placeat esse dolorem pariatur? Nihil sapiente quod repellendus laborum praesentium enim! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur explicabo veritatis dolores sint voluptatibus ex, laboriosam assumenda saepe beatae nemo impedit nulla illum molestias nam, quam voluptatum. Inventore, numquam laborum!</p>
+
+      <img src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg" className="img1" />
+      <img src="[img]" alt="img" className='img1' />
+
+    </header>
+    </>
+  );
+}
+
+export default App;

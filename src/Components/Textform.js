b9204ae0e353dc975a0d1042a5c423112db8bc47
krupalidholakiya <krupalidholakiya85@gmail.com>
2024-06-06 16:44:53 +0530

    Text to Upper Case Letter

--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -1,13 +1,23 @@
 import React,{useState} from 'react'
 
 export default function Textform(props) {
-    var [text, setText] = useState("Input Your text");
+    const [text, setText] = useState("Input Your text");
+    // setText("Entrer Your Text");
+
+    const handleOnChange = (event) => {
+      setText(event.target.value)
+    }
+
+    const handleUpChange = () => {
+      let newText =  text.toUpperCase();
+      setText(newText);
+    }
   return (
     <div className='mb-3 mx-5 my-5'>
         <h1>{props.heading}</h1>
     
-  <textarea className="form-control" id="exampleFormControlTextarea1" rows="8" value={text}></textarea>
-  <button className='btn btn-outline-success my-3'>Convert to Upper Case</button>
+  <textarea className="form-control" id="exampleFormControlTextarea1" rows="8" value={text} onChange={handleOnChange}></textarea>
+  <button className='btn btn-outline-success my-3' onClick={handleUpChange}>Convert to Upper Case</button>
 </div>
   )
 }

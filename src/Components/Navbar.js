3a9f25f9fc4d74d92a1e3bebbdd7158cf416268c
krupalidholakiya <krupalidholakiya85@gmail.com>
2024-06-08 17:36:16 +0530

    better navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,47 +1,33 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
 export default function Navbar(props) {
   return (
     <div>
-       
-       <nav className={`navbar navbar-expend-lg bg-${props.mode} navbar-${props.mode}`}>
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">{props.title}</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">{props.title}</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">{props.aboutText}</a>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
         </li>
-        <li className="nav-item dropdown" />
-          </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <div className="form-check form-switch">
-  <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={props.toogleMode}/> {props.mode}
-</div>
-
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
     </div>
   </div>
 </nav>
     </div>
   )
-}
-
-
-Navbar.propTypes={
-    title : PropTypes.string.isRequired,
-    aboutText : PropTypes.string
-}
-
-Navbar.defaultProps={
-    title : "Set Your Title Here",
-    aboutText : "set your text here",
 }
